0aa02f2022451aad7bd5c22df62012c9b18783b1
aleksandrstarzhynskyi <a.stargik@gmail.com>
2023-10-31 15:01:12 +0200

    Initial Commit

---
+++ LexYaccParser/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LexYaccParser
+//
+//  Created by Aleksandr Starzhynskyi on 31.10.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
